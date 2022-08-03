84389127bf6b470182343c67c2d6fbb30f9f1186
Donathan Ramalho <97134195+donathanramalho@users.noreply.github.com>
2022-08-02 21:29:42 -0300

    Add files via upload

---
+++ interfaces/Embaralhador.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface Embaralhador {
+	
+	// Cada classe de embaralhar palavras terá esse método
+	public String palavraEmbaralhada(String palavra);
+	
+}
---
+++ interfaces/MecanicaDoJogo.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface MecanicaDoJogo {
+	
+	// Cada classe com alguma mecânica terá um jeito diferente
+	// de retornar cada resposta do usuário com esse método
+	public int posJogada(String chute);
+	
+}
